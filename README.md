b3d5cd02f28734112e5fd29e3369b3f4fe3639e5
stereobooster <stereobooster@gmail.com>
2018-09-04 22:21:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Experiments with modal dialog
+
+- https://ui.reach.tech/dialog
+- https://github.com/theKashey/react-scroll-locky
+- https://github.com/stereobooster/mobile-safari-fullscreen
+
+Other:
+
+- https://github.com/theKashey/react-focus-lock
+- https://github.com/theKashey/react-locky
+
+About focus https://gist.github.com/cvrebert/68659d0333a578d75372
---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React from "react";
+import "./App.css";
+
+import Component from "@reach/component-component";
+import { Dialog } from "@reach/dialog";
+import { ScrollLocky } from "react-scroll-locky";
+import FullScreen from "mobile-safari-fullscreen";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Component initialState={{ showDialog: false }}>
+        {({ state, setState }) => (
+          <div className="content">
+            <form>
+              <input />
+            </form>
+
+            <p>Scroll down</p>
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <button onClick={() => setState({ showDialog: true })}>
+              Show Dialog
+            </button>
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            {state.showDialog && (
+              <FullScreen
+                classNames={{ iosFix: "iosFix" }}
+                isOpen={state.showDialog}
+              >
+                <Dialog className="fullscreen">
+                  <ScrollLocky>
+                    <form>
+                      <input />
+                    </form>
+                    <button onClick={() => setState({ showDialog: false })}>
+                      Close
+                    </button>
+                    <p>
+                      imagine long list from combox, like downshift
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      <br />
+                      ...more
+                    </p>
+                  </ScrollLocky>
+                </Dialog>
+              </FullScreen>
+            )}
+          </div>
+        )}
+      </Component>
+    );
+  }
+}
+
+export default App;
